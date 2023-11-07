d9ebcc5ec1e431ddd08d05278c861904f43da072
navyapokuri <navya.pokuri@gmail.com>
2023-11-06 19:12:27 -0500

    added a review page

---
+++ xmlproject/xmlproject/Pages/ReviewForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace xmlproject.Pages
+{
+    public class ReviewFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
