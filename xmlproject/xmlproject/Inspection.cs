75bcc879647d679961585a9ef21b605ab300b089
ishaan chaudhari <chaudhia@mail.uc.edu>
2023-11-10 09:11:54 -0500

    code review by chaudhia

--- xmlproject/xmlproject/Inspection.cs
+++ xmlproject/xmlproject/Inspection.cs
@@ -124,7 +124,7 @@ namespace InspectionData
             {
                 return l;
             }
-            throw new Exception("Cannot unmarshal type long");
+            throw new JsonReaderException("Cannot unmarshal type long");
         }
 
         public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
